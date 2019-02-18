d76c946b66d4a3c2244cc0627a08a29ee127062a
gfike19@gmail.com <Vaughn*573>
2019-02-18 16:20:07 -0600

    started work on mindtap for unit 4

--- chapter4.py
+++ chapter4.py
@@ -1,9 +1,7 @@
-# import os
-
 # print("Exercise 1")
 # oldText = input("Enter some text: ")
 # dist = int(input("Enter rotation value: "))
-
+#
 # newText = ""
 # for each in oldText:
 #     oldValue = ord(each)
@@ -11,7 +9,7 @@
 #     newChar = chr(newValue)
 #     newText += newChar
 # print(newText)
-
+#
 # print("\n")
 # print("Exercise 2")
 # oldText = input("Enter encrypted text: ")
@@ -23,7 +21,7 @@
 #     newChar = chr(newValue)
 #     newText += newChar
 # print(newText)
-
+#
 # print("\n")
 # print("Exercise 3")
 # oldFileName = input("Enter the input filename: ")
@@ -44,11 +42,11 @@
 # for each in newText:
 #     newFile.write(each)
 # newFile.close()
-
+#
 # print("\n")
 # print("Exercise 4")
 # decimal = int(input("Enter a decimal integer: "))
-# if decimal == 0: 
+# if decimal == 0:
 #     print(0)
 # else:
 #     print("Quotient Remainder Octal")
@@ -58,8 +56,8 @@
 #         decimal = decimal // 8
 #         ostring = str(remainder) + bstring
 #         print("%5d%8d%12s" % (decimal, remainder, ostring))
-#     print("The octal representation is", ostring)  
-
+#     print("The octal representation is", ostring)
+#
 # ostring = input("Enter a string of octal digits: ")
 # decimal = 0
 # exponent = len(ostring) - 1
@@ -67,16 +65,16 @@
 #     decimal = decimal + int(digit) * 8 ** exponent
 #     exponent = exponent - 1
 # print("The integer value is", decimal)
-
+#
 # print("\n")
 # print("Exercise 5")
-
+#
 # print("\n")
 # print("Exercise 6")
-
+#
 # print("\n")
 # print("Exercise 7")
-
+#
 # print("\n")
 # print("Exercise 8")
 # oldFileName = input("Enter the input file name: ")
@@ -90,7 +88,7 @@
 # for each in text:
 #     newFile.write(each)
 # newFile.close()
-
+#
 # print("\n")
 # print("Exercise 9")
 # oldFileName = input("Enter the input file name: ")
@@ -106,27 +104,41 @@
 #     newLine = str(counter) + ">" + each
 #     newFile.write(newLine)
 # newFile.close()
-
+#
 # print("\n")
 # print("Exercise 10")
-fileOneName = input("Enter the input file name: ")
-fileTwoName = input("Enter the output file name: ")
-fileOneText = ""
-fileTwoText = ""
-fileOne = open(fileOneName, "r")
-for each in fileOne:
-    fileOneText += each
-fileOne.close()
-fileTwo = open(fileTwoName, "r")
-for each in fileTwo:
-    fileTwoText += each
-fileTwo.close()
+# fileOneName = input("Enter the input file name: ")
+# fileTwoName = input("Enter the output file name: ")
+# fileOneText = ""
+# fileTwoText = ""
+# fileOne = open(fileOneName, "r")
+# for each in fileOne:
+#     fileOneText += each
+# fileOne.close()
+# fileTwo = open(fileTwoName, "r")
+# for each in fileTwo:
+#     fileTwoText += each
+# fileTwo.close()
+#
+# counter = 0
+# for each in fileOneText:
+#     if each != fileTwoText[counter]:
+#         print("No " + each)
+#         break
+#     else:
+#         counter += 1
+# print("Yes")
 
-counter = 0
-for each in fileOneText:
-    if each != fileTwoText[counter]:
-        print("No " + each)
-        break
-    else:
-        counter += 1
-print("Yes")
+# Exercise 12
+fileName = input("Enter the file name: ")
+data = open(fileName, "r")
+text = []
+for each in data:
+    text.append(each.split(" "))
+data.close()
+for line in text:
+    wages = float(line[1])
+    hrs = float(line[2])
+    name = line[0]
+    pay = float(wages) * float(hrs)
+    print(name, hrs, pay)
